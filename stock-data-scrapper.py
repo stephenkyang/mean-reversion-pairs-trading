3df3097e234ac6c099a3eabd19528910ca26b26a
Stephen Yang <stevyk6@gmail.com>
2020-12-21 12:30:29 -0800

    updated scrapper

--- stock-data-scrapper.py
+++ stock-data-scrapper.py
@@ -13,7 +13,7 @@ class yFinanceScrapper(object):
             yFinanceScrapper.ticker_data[ticker] = data
 
 
-data = pd.read_csv("/Users/stephen/Desktop/Pairs Trading Project/data.csv")
+data = pd.read_csv("/Users/stephen/Desktop/Pairs Trading Project/ticker-names-on-NASDAQ-NYSE.csv")
 data = data[data["Volume"] > 10000][data["IPO Year"] < 2010][data["Symbol"] != "AMHC"]["Symbol"]
 
 #for real time use
@@ -23,7 +23,7 @@ print(yFinanceScrapper.ticker_data)
 #for storing testing data
 
 ticker_names = ""
-for ticker in data:
+for ticker in data[0:10]:
     ticker_names += ticker + " "
 ticker_download = yf.download(ticker_names, start="2010-01-01", end="2020-01-01", group_by="ticker")
 ticker_download.to_csv("historical-ticker-data.csv", index = False)
