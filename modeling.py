f9eeeceb77388fbeb3248a8134439ba00569840f
Stephen Yang <stevyk6@gmail.com>
2020-12-21 23:27:14 -0800

    Added modeling libraries

--- modeling.py
+++ modeling.py
@@ -1,2 +1,10 @@
-import statsmodel
+import numpy as np
 import pandas as pd
+import statsmodels
+np.random.seed(87)
+import matplotlib.pyplot as plt
+Xreturns = np.random.normal(0, 1, 100)
+# sum them and shift all the prices up
+X = pd.Series(np.cumsum(Xreturns), name='X') + 50
+X.plot(figsize=(15,7))
+plt.show()
