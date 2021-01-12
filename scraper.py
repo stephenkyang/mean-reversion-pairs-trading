77b3d828615b354240cc3e957d4bbedfdf79ae78
Stephen Yang <stevyk6@gmail.com>
2021-01-12 01:14:17 -0800

    final commit

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ class yFinanceScraper(object):
     namelist = []
     first = True
     df = None
-    period = "1300"
+    period = "1415"
     def __init__(self,names):
         self.names = names
         self.historicallist= []
